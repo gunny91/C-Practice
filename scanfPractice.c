1a8d24a06de1d835c75fff1423f0c86256944b2c
gunny <gunny91@iastate.edu>
2021-08-16 08:32:21 +0900

    scanf

---
+++ scanfPractice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define _CRT_SERCURE_NO_WARNINGS
+
+int main()
+{
+  int n1;
+
+  printf("Enter the integeger  value: ");
+  scanf("%d", &n1);
+
+  printf("%d\n",n1);
+  
+  return 0;
+}
